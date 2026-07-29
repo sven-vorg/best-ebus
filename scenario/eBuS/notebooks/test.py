19618d8dc38cd2f3b4fa06a2e193e769c581a3de
sven-vorg <sven.vorg@gmail.com>
2026-07-29 23:54:14 +0200

    feat: simple evaluations

--- scenario/eBuS/notebooks/test.py
+++ scenario/eBuS/notebooks/test.py
@@ -1,6 +1,6 @@
 import lxml.etree as etree
 
-tree = etree.parse(r"G:\Dokumente\Studium\FU Berlin\BeSTeBuS\best-ebus\scenario\sumo\output\electric_bus_2026-07-29-20-13-37_chargingstations.xml")
+tree = etree.parse(r"G:\Dokumente\Studium\FU Berlin\BeSTeBuS\best-ebus\scenario\sumo\output\electric_bus_2026-07-29-23-19-57_chargingstations.xml")
 root = tree.getroot()
 count = len(root.findall("chargingEvent"))
 print(count)
